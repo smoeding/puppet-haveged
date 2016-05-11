6f7d8148093efbf865ca068b5e0a41914f4b55d6
Stefan Möding <stm@kill-9.net>
2016-05-11 18:09:39 +0200

    Use context in fact tests

--- spec/unit/facter/haveged_startup_provider_spec.rb
+++ spec/unit/facter/haveged_startup_provider_spec.rb
@@ -9,14 +9,14 @@ describe 'Facter::Util::Fact' do
     Facter.clear
   }
 
-  describe 'haveged_startup_provider with /proc/1/comm' do
+  context 'haveged_startup_provider with /proc/1/comm' do
     it {
       File.stubs(:open).returns("foo\n")
       expect(Facter.fact(:haveged_startup_provider).value).to eq('foo')
     }
   end
 
-  describe 'haveged_startup_provider without /proc/1/comm' do
+  context 'haveged_startup_provider without /proc/1/comm' do
     it {
       File.stubs(:open) { raise(StandardException) }
       expect(Facter.fact(:haveged_startup_provider).value).to eq('init')
