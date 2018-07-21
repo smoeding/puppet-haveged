447015be7da81bc09a767ab95d82f5b5cb0492a6
Stefan Möding <stm@kill-9.net>
2018-07-21 20:07:47 +0200

    Rewrite facts provisioning for tests

--- spec/classes/package_spec.rb
+++ spec/classes/package_spec.rb
@@ -3,11 +3,21 @@ require 'spec_helper'
 describe 'haveged::package' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
-      before(:each) do
-        Facter.clear
-        facts.each do |k, v|
-          Facter.stubs(:fact).with(k).returns Facter.add(k) { setcode { v } }
-        end
+      let(:facts) do
+        facts
+      end
+
+      osrel = facts[:operatingsystemmajrelease] || facts[:operatingsystemrelease]
+      osver = "#{facts[:operatingsystem]}-#{osrel}"
+
+      case osver
+      when 'Ubuntu-14.04'
+        let(:facts) { facts.merge(haveged_startup_provider: 'init') }
+      when 'Scientific-6', 'CentOS-6', 'RedHat-6', 'OracleLinux-6'
+        let(:facts) { facts.merge(haveged_startup_provider: 'init') }
+      when 'Debian-8', 'Debian-9', 'Ubuntu-16.04', 'Ubuntu-18.04',
+           'Scientific-7', 'CentOS-7', 'RedHat-7', 'OracleLinux-7'
+        let(:facts) { facts.merge(haveged_startup_provider: 'systemd') }
       end
 
       context 'with default parameters' do
