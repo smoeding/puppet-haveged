0ddc940b161818cc59a3cd94f710678aa2fed317
Stefan Möding <stm@kill-9.net>
2020-10-27 20:29:09 +0100

    Update test for fact

--- spec/unit/facter/haveged_startup_provider_spec.rb
+++ spec/unit/facter/haveged_startup_provider_spec.rb
@@ -1,26 +1,31 @@
 require 'spec_helper'
+require 'facter/haveged_startup_provider'
 
-describe 'Facter::Util::Fact' do
-  subject { Facter.fact(:haveged_startup_provider).value }
+describe 'haveged_startup_provider', type: :fact do
+  before(:each) { Facter.clear }
+  after(:each) { Facter.clear }
 
-  before(:each) do
-    Facter.clear
-    Facter.fact(:kernel).stub(:value) { 'Linux' }
-  end
-
-  context 'haveged_startup_provider with /proc/1/comm' do
+  context 'when haveged_startup_provider with /proc/1/comm' do
     before(:each) do
-      allow(File).to receive(:open).and_return("foo\n")
+      allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
+      allow(File).to receive(:open).and_call_original
+      allow(File).to receive(:open).with('/proc/1/comm').and_return("foo\n")
     end
 
-    it { is_expected.to eq('foo') }
+    it {
+      expect(Facter.fact(:haveged_startup_provider).value).to eq('foo')
+    }
   end
 
-  context 'haveged_startup_provider without /proc/1/comm' do
+  context 'when haveged_startup_provider without /proc/1/comm' do
     before(:each) do
-      allow(File).to receive(:open).and_raise(StandardError)
+      allow(Facter.fact(:kernel)).to receive(:value).and_return('Linux')
+      allow(File).to receive(:open).and_call_original
+      allow(File).to receive(:open).with('/proc/1/comm').and_raise(StandardError)
     end
 
-    it { is_expected.to eq('init') }
+    it {
+      expect(Facter.fact(:haveged_startup_provider).value).to eq('init')
+    }
   end
 end
